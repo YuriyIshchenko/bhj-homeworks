e16ed98f9630605083f26e6d810436e8afae763d
Yuriy Ishchenko <iuaishchenko@gmail.com>
2024-03-07 06:36:30 +0300

    Задание 2.3.3 выполнено

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tooltips = Array.from(document.querySelectorAll('.has-tooltip'));
+
+    tooltips.forEach((tooltip) => {
+        let tooltipItem = null;
+        tooltip.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (!tooltipItem) {
+                const position = tooltip.getBoundingClientRect();
+                tooltip.insertAdjacentHTML('afterend', `<div class="tooltip" style="left: ${parseInt(position.left)}px;">${tooltip.getAttribute('title')}</div>`);
+                tooltipItem = tooltip.nextElementSibling;
+            }
+            tooltipItem.classList.toggle('tooltip_active');
+        });
+    });
+});
+s
